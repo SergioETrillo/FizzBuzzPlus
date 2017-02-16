75eb368893c2ceba42a3fc29c0ba34944d465e3a
Sergio Enrech Trillo <senrech@yahoo.fr>
2017-02-16 17:00:41 +0000

    initial commit

---
+++ FizzBuzzPlus.Tests/FizzBuzzPlusTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using FizzBuzzPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HundredDoors.Tests
+{
+    [TestFixture]
+    public class FizzBuzzPlusTests
+    {
+        [Test]
+        public void ShouldReturnANumberIfNotMultipleOf3Or5()
+        {
+            Assert.That(Program.FizzBuzzPlus(7), Is.EqualTo("7"));
+        }
+
+        [Test]
+        public void ShouldReturnFizzForMultiplesOf3()
+        {
+            Assert.That(Program.FizzBuzzPlus(3), Is.EqualTo("...Fizz..."));
+        }
+
+        [Test]
+        public void ShouldReturnBuzzForMultiplesOf5()
+        {
+            Assert.That(Program.FizzBuzzPlus(5), Is.EqualTo("...Buzz..."));
+        }
+
+        [Test]
+        public void ShouldReturnFizzIfNumberContains3(
+                    [Values(13,23,31,32,33,34,36,37,38,39,43,63,73,83,93)] int x)
+        {
+            Assert.That(Program.FizzBuzzPlus(x), Is.EqualTo("...Fizz..."));
+        }
+
+        [Test]
+        public void ShouldReturnBuzzIfNumberContains5(
+                    [Values (52,56,58,59)] int x)
+        {
+            Assert.That(Program.FizzBuzzPlus(x), Is.EqualTo("...Buzz..."));
+        }
+
+        [Test]
+        public void ShouldReturnFizzBuzzIfNumberContains3Or5OrMultipleOf3OrMultipleOf5(
+            [Values(35, 51, 53, 54, 57)] int x)
+        {
+            Assert.That(Program.FizzBuzzPlus(x), Is.EqualTo("FizzBuzz"));
+        }
+    }
+}
---
+++ FizzBuzzPlus/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FizzBuzzPlus
+{
+    public class Program
+    {
+        public static string FizzBuzzPlus(int n)
+        {
+            if (MultipleOf3AndContains5(n)
+                || MultipleOf5AndContains3(n)
+                || HasThree(n) && HasFive(n))
+                return "FizzBuzz";
+
+            if (n % 3 == 0 || HasThree(n))
+                return "...Fizz...";
+
+            if (n % 5 == 0 || HasFive(n))
+            {
+                return "...Buzz...";
+            }
+
+            return n.ToString();
+        }
+
+        private static bool MultipleOf3AndContains5(int n)
+        {
+            return (n % 3 == 0 && HasFive(n));
+        }
+
+        private static bool MultipleOf5AndContains3(int n)
+        {
+            return (n % 5 == 0 && HasThree(n));
+        }
+
+        private static bool HasFive(int n)
+        {
+            return n.ToString().Contains("5");
+        }
+
+        private static bool HasThree(int n)
+        {
+            return n.ToString().Contains("3");
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i= 1; i<=100; i++)
+            {
+                Console.WriteLine(FizzBuzzPlus(i));
+            }
+            Console.Read();
+        }
+    }
+}
